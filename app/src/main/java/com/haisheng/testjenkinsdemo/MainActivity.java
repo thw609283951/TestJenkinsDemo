238bf6f1bbd43fc357dad974b167c0da460b9b3b
thw <thw609283951@163.com>
2018-09-26 17:41:54 +0800

    添加 ProgressWebView  加载远程链接

--- app/src/main/java/com/haisheng/testjenkinsdemo/MainActivity.java
+++ app/src/main/java/com/haisheng/testjenkinsdemo/MainActivity.java
@@ -5,9 +5,28 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressWebView webView;
+
+    private String mUrl = "http://zs1.aada.top/app/index.php?i=6&c=entry&do=shop&m=sz_yi";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setLoadListener(new ProgressWebView.OnLoadListener() {
+            @Override
+            public void loadSuccess() {
+
+            }
+
+            @Override
+            public void loadError() {
+
+            }
+        });
+
+        webView.loadUrl(mUrl);
     }
 }
